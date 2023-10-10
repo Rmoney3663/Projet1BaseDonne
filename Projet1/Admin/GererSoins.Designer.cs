c486510ebd51f023c3a443888b9b82b8671da1d1
Chipsy0 <99271702+Chipsy0@users.noreply.github.com>
2023-10-10 19:40:24 -0400

    Supprimer Soin FINI

--- Projet1/Admin/GererSoins.Designer.cs
+++ Projet1/Admin/GererSoins.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.b56Projet1Equipe7DataSet = new Projet1.B56Projet1Equipe7DataSet();
             this.soinBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.soinTableAdapter = new Projet1.B56Projet1Equipe7DataSetTableAdapters.soinTableAdapter();
@@ -39,15 +39,31 @@
             this.btnModifier = new System.Windows.Forms.Button();
             this.btnAjout = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnFermer = new System.Windows.Forms.Button();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.assistantSoinBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.assistantSoinTableAdapter = new Projet1.B56Projet1Equipe7DataSetTableAdapters.assistantSoinTableAdapter();
+            this.assistantSoinDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.planifSoinBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.planifSoinTableAdapter = new Projet1.B56Projet1Equipe7DataSetTableAdapters.planifSoinTableAdapter();
+            this.planifSoinDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.b56Projet1Equipe7DataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.soinBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgSoins)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assistantSoinBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assistantSoinDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.planifSoinBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.planifSoinDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // b56Projet1Equipe7DataSet
@@ -87,14 +103,14 @@
             this.dgSoins.AllowUserToDeleteRows = false;
             this.dgSoins.AutoGenerateColumns = false;
             this.dgSoins.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgSoins.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgSoins.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dgSoins.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgSoins.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn1,
@@ -166,46 +182,6 @@
             this.label1.TabIndex = 26;
             this.label1.Text = "Soins";
             // 
-            // dataGridViewTextBoxColumn1
-            // 
-            this.dataGridViewTextBoxColumn1.DataPropertyName = "noSoin";
-            this.dataGridViewTextBoxColumn1.HeaderText = "Numéro soin";
-            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-            this.dataGridViewTextBoxColumn1.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn2
-            // 
-            this.dataGridViewTextBoxColumn2.DataPropertyName = "description";
-            this.dataGridViewTextBoxColumn2.HeaderText = "Description";
-            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
-            this.dataGridViewTextBoxColumn2.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn3
-            // 
-            this.dataGridViewTextBoxColumn3.DataPropertyName = "duree";
-            this.dataGridViewTextBoxColumn3.HeaderText = "Durée";
-            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
-            this.dataGridViewTextBoxColumn3.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn4
-            // 
-            this.dataGridViewTextBoxColumn4.DataPropertyName = "noTypeSoin";
-            this.dataGridViewTextBoxColumn4.HeaderText = "Numéro type soin";
-            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
-            this.dataGridViewTextBoxColumn4.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn5
-            // 
-            this.dataGridViewTextBoxColumn5.DataPropertyName = "prix";
-            this.dataGridViewTextBoxColumn5.HeaderText = "Prix";
-            this.dataGridViewTextBoxColumn5.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
-            this.dataGridViewTextBoxColumn5.ReadOnly = true;
-            // 
             // btnFermer
             // 
             this.btnFermer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
@@ -220,11 +196,153 @@
             this.btnFermer.UseVisualStyleBackColor = false;
             this.btnFermer.Click += new System.EventHandler(this.btnFermer_Click);
             // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "prix";
+            this.dataGridViewTextBoxColumn5.HeaderText = "Prix";
+            this.dataGridViewTextBoxColumn5.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "noTypeSoin";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Numéro type soin";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "duree";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Durée";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "description";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Description";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "noSoin";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Numéro soin";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // assistantSoinBindingSource
+            // 
+            this.assistantSoinBindingSource.DataMember = "fk_noSoinAssistantSoin";
+            this.assistantSoinBindingSource.DataSource = this.soinBindingSource;
+            // 
+            // assistantSoinTableAdapter
+            // 
+            this.assistantSoinTableAdapter.ClearBeforeFill = true;
+            // 
+            // assistantSoinDataGridView
+            // 
+            this.assistantSoinDataGridView.AutoGenerateColumns = false;
+            this.assistantSoinDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.assistantSoinDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7});
+            this.assistantSoinDataGridView.DataSource = this.assistantSoinBindingSource;
+            this.assistantSoinDataGridView.Location = new System.Drawing.Point(514, 236);
+            this.assistantSoinDataGridView.Name = "assistantSoinDataGridView";
+            this.assistantSoinDataGridView.RowHeadersWidth = 51;
+            this.assistantSoinDataGridView.RowTemplate.Height = 24;
+            this.assistantSoinDataGridView.Size = new System.Drawing.Size(292, 39);
+            this.assistantSoinDataGridView.TabIndex = 29;
+            this.assistantSoinDataGridView.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "noAssistant";
+            this.dataGridViewTextBoxColumn6.HeaderText = "noAssistant";
+            this.dataGridViewTextBoxColumn6.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.DataPropertyName = "noSoin";
+            this.dataGridViewTextBoxColumn7.HeaderText = "noSoin";
+            this.dataGridViewTextBoxColumn7.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.Width = 125;
+            // 
+            // planifSoinBindingSource
+            // 
+            this.planifSoinBindingSource.DataMember = "fk_noSoinPlanifSoin";
+            this.planifSoinBindingSource.DataSource = this.soinBindingSource;
+            // 
+            // planifSoinTableAdapter
+            // 
+            this.planifSoinTableAdapter.ClearBeforeFill = true;
+            // 
+            // planifSoinDataGridView
+            // 
+            this.planifSoinDataGridView.AutoGenerateColumns = false;
+            this.planifSoinDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.planifSoinDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn8,
+            this.dataGridViewTextBoxColumn9,
+            this.dataGridViewTextBoxColumn10,
+            this.dataGridViewTextBoxColumn11});
+            this.planifSoinDataGridView.DataSource = this.planifSoinBindingSource;
+            this.planifSoinDataGridView.Location = new System.Drawing.Point(514, 281);
+            this.planifSoinDataGridView.Name = "planifSoinDataGridView";
+            this.planifSoinDataGridView.RowHeadersWidth = 51;
+            this.planifSoinDataGridView.RowTemplate.Height = 24;
+            this.planifSoinDataGridView.Size = new System.Drawing.Size(292, 36);
+            this.planifSoinDataGridView.TabIndex = 29;
+            this.planifSoinDataGridView.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.DataPropertyName = "noPersonne";
+            this.dataGridViewTextBoxColumn8.HeaderText = "noPersonne";
+            this.dataGridViewTextBoxColumn8.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            this.dataGridViewTextBoxColumn8.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn9
+            // 
+            this.dataGridViewTextBoxColumn9.DataPropertyName = "noAssistant";
+            this.dataGridViewTextBoxColumn9.HeaderText = "noAssistant";
+            this.dataGridViewTextBoxColumn9.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
+            this.dataGridViewTextBoxColumn9.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn10
+            // 
+            this.dataGridViewTextBoxColumn10.DataPropertyName = "dateHeure";
+            this.dataGridViewTextBoxColumn10.HeaderText = "dateHeure";
+            this.dataGridViewTextBoxColumn10.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
+            this.dataGridViewTextBoxColumn10.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn11
+            // 
+            this.dataGridViewTextBoxColumn11.DataPropertyName = "noSoin";
+            this.dataGridViewTextBoxColumn11.HeaderText = "noSoin";
+            this.dataGridViewTextBoxColumn11.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn11.Name = "dataGridViewTextBoxColumn11";
+            this.dataGridViewTextBoxColumn11.Width = 125;
+            // 
             // GererSoins
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1035, 484);
+            this.ClientSize = new System.Drawing.Size(1035, 493);
+            this.Controls.Add(this.planifSoinDataGridView);
+            this.Controls.Add(this.assistantSoinDataGridView);
             this.Controls.Add(this.btnFermer);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSupprimer);
@@ -238,6 +356,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.b56Projet1Equipe7DataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.soinBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgSoins)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assistantSoinBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assistantSoinDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.planifSoinBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.planifSoinDataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -254,11 +376,23 @@
         private System.Windows.Forms.Button btnModifier;
         private System.Windows.Forms.Button btnAjout;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFermer;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
-        private System.Windows.Forms.Button btnFermer;
+        private System.Windows.Forms.BindingSource assistantSoinBindingSource;
+        private B56Projet1Equipe7DataSetTableAdapters.assistantSoinTableAdapter assistantSoinTableAdapter;
+        private System.Windows.Forms.DataGridView assistantSoinDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.BindingSource planifSoinBindingSource;
+        private B56Projet1Equipe7DataSetTableAdapters.planifSoinTableAdapter planifSoinTableAdapter;
+        private System.Windows.Forms.DataGridView planifSoinDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn11;
     }
 }
--- Projet1/Admin/GererSoins.cs
+++ Projet1/Admin/GererSoins.cs
@@ -17,16 +17,14 @@ namespace Projet1.Admin
             InitializeComponent();
         }
 
-        private void soinBindingNavigatorSaveItem_Click(object sender, EventArgs e)
-        {
-            this.Validate();
-            this.soinBindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.b56Projet1Equipe7DataSet);
-
-        }
-
         private void GererSoins_Load(object sender, EventArgs e)
         {
+            // TODO: cette ligne de code charge les données dans la table 'b56Projet1Equipe7DataSet.planifSoin'. Vous pouvez la déplacer ou la supprimer selon les besoins.
+            this.planifSoinTableAdapter.Fill(this.b56Projet1Equipe7DataSet.planifSoin);
+            // TODO: cette ligne de code charge les données dans la table 'b56Projet1Equipe7DataSet.assistantSoin'. Vous pouvez la déplacer ou la supprimer selon les besoins.
+            this.assistantSoinTableAdapter.Fill(this.b56Projet1Equipe7DataSet.assistantSoin);
+            // TODO: cette ligne de code charge les données dans la table 'b56Projet1Equipe7DataSet.assistantSoin'. Vous pouvez la déplacer ou la supprimer selon les besoins.
+            this.assistantSoinTableAdapter.Fill(this.b56Projet1Equipe7DataSet.assistantSoin);
             // TODO: cette ligne de code charge les données dans la table 'b56Projet1Equipe7DataSet.soin'. Vous pouvez la déplacer ou la supprimer selon les besoins.
             this.soinTableAdapter.Fill(this.b56Projet1Equipe7DataSet.soin);
 
@@ -98,7 +96,57 @@ namespace Projet1.Admin
 
         private void btnSupprimer_Click(object sender, EventArgs e)
         {
+            if (dgSoins.SelectedRows.Count == 1 && dgSoins.SelectedRows[0].IsNewRow == false)
+            {
+                // Vérification si le soin est planifié
 
+                decimal noSoin = (decimal)dgSoins.SelectedRows[0].Cells[0].Value;
+
+                bool booPlanifie = false;
+                foreach (B56Projet1Equipe7DataSet.planifSoinRow uneLigne in b56Projet1Equipe7DataSet.planifSoin.Rows)
+                {
+                    if (uneLigne.noSoin == noSoin)
+                    {
+                        MessageBox.Show("Ce soin ne peut pas être supprimé, car il est déjà planifié.",
+                            "Supprimer soin avec planification", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        booPlanifie = true;
+                        break;
+                    }
+                }
+
+                if (booPlanifie == false)
+                {
+                    bool booAssistant = false;
+                    foreach (B56Projet1Equipe7DataSet.assistantSoinRow uneLigne in b56Projet1Equipe7DataSet.assistantSoin.Rows)
+                    {
+                        if (uneLigne.noSoin == noSoin)
+                        {
+                            MessageBox.Show("Ce soin ne peut pas être supprimé, car il est présentement offert par un assistant.",
+                                "Supprimer soin avec assistant", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            booAssistant = true;
+                            break;
+                        }
+                    }
+
+                    if (booAssistant == false)
+                    {
+                        DialogResult reponse = MessageBox.Show("Êtes-vous certain de vouloir supprimer le soin numéro " + noSoin + "?",
+                            "Supprimer un soin", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                        if (reponse == DialogResult.Yes)
+                        {
+                            soinBindingSource.RemoveCurrent();
+                            this.Validate();
+                            this.soinBindingSource.EndEdit();
+                            this.soinTableAdapter.Update(this.b56Projet1Equipe7DataSet.soin);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner le soin que vous souhaitez supprimer dans la liste.", "Sélection d'un soin", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnFermer_Click(object sender, EventArgs e)
